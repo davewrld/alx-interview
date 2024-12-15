e1988b6dc868e7d9e3754e2ad457c232036122ff
davewrld <keithdave23@gmail.com>
2024-12-15 15:52:10 -0800

    fixxed documentation errors

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
+"""
+Algorithim that dertermines winner of strategic
+prime number game.
+"""
+
+
 def is_prime(n):
-    """Check if a number is prime"""
+    """
+    Check if a number is prime
+    """
     if n < 2:
         return False
+
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
@@ -13,14 +22,18 @@ def is_prime(n):
 def play_game(n):
     """
     Stimulate a single game from 1 to n.
-    Returns True if Maria wins, False if Ben wins.
-    """
 
-    # available numbers set
+    Args:
+        n(int)
+
+    Returns:
+        bool: True if Maria wins, False if Ben wins.
+    """
     available = set(range(1, n + 1))
     maria_turn = True
 
     while True:
+
         primes = [p for p in available if is_prime(p)]
 
         if not primes:
@@ -30,22 +43,19 @@ def play_game(n):
 
         to_remove = {p for p in available if p % prime == 0}
         available -= to_remove
-        # next palyer turn
+
         maria_turn = not maria_turn
 
 
 def isWinner(x, nums):
     """
-    Overall winner across x rounds.
-
+    Determine overall winner.
     Args:
-    x (int): Number of rounds
-    nums (list): Set of n for each round
-
+        x(int): Number of rounds
+        nums(list)
     Returns:
-    str: Name of the player who won most rounds, or None
+        str:name of winner.
     """
-
     maria_wins = 0
     ben_wins = 0
 
